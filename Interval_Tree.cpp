cbaffcaa76634bb6e21fd6d4bef1827a50efffb4
Akash Gupta <sky.gup7@gmail.com>
2017-02-15 21:25:18 +0530

    Second
    basic Input

--- Interval_Tree.cpp
+++ Interval_Tree.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define LLMAX 1000000000000000010
+#define MAX 100005
+#define gcd __gcd
+#define mod 1000000007
+#define pb(n) push_back(n)
+#define fr front()
+#define vint vector<int>
+#define vstring vector<string>
+#define vll vector<long long int>
+#define forp(i,a,n) for(int i=(a);i<(n);i++)
+#define forn(i,a,n) for(int i=(a);i>=(n);i--)
+#define csl ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+int main()
+{
+    csl;
+    int n,x,y;
+    cin>>n;
+    int interval[2][n];
+    forp(i,0,n)
+    {
+        cin>>x>>y;
+        interval[0][i]=x;
+        interval[1][i]=y;
+    }
+}
+
 
